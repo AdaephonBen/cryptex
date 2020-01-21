09f84d0dae4368e362fd56d6e60a66a79f70715c
deepikapalagani <es18btech11002@iith.ac.in>
2020-01-21 21:27:46 +0530

    updated

--- src/Components/LevelTemplates/index.js
+++ src/Components/LevelTemplates/index.js
@@ -11,7 +11,7 @@ const data = [
   {
     key: '1',
     firstName: 1,
-    lastName: 'Brown',
+    lastName: 'yppppppppppppppppppppppppppppppp',
     age: 32,
   },
   {
@@ -48,19 +48,19 @@ const data = [
 
 const new_data = [
   {
-    title: 'Ant Design Title 1',
+    title: 'Hint 1',
     description: 'yppppppppppppppppppppppppppppppp',
   },
   {
-    title: 'Ant Design Title 2',
+    title: 'Hint 2',
     description: 'yppppppppppppppppppppppppppppppp',
   },
   {
-    title: 'Ant Design Title 3',
+    title: 'Hint 3',
     description: 'yppppppppppppppppppppppppppppppp',
   },
   {
-    title: 'Ant Design Title 4',
+    title: 'Hint 4',
     description: 'yppppppppppppppppppppppppppppppp',
   },
 ];
@@ -69,15 +69,25 @@ const customPanelStyle1 = {
   overflow: 'hidden',
   color: '#cff8fa',
   background: '#13242b',
-  borderTop: "solid .05vw #cff8fa",
-  borderBottom: "solid .05vw #cff8fa",
-  marginBottom: '.2vw'
+  borderTop: "solid .05vw #2b3d43",
+  borderBottom: "solid .05vw #2b3d43",
+  borderRadius: '0px'
 };
 const customPanelStyle2 = {
   overflow: 'hidden',
   color: '#cff8fa',
   background: '#13242b',
-  borderBottom: "solid .05vw #cff8fa",
+  borderBottom: "solid .05vw #2b3d43",
+  borderRadius: '0px'
+};
+const customPanelStyle3 = {
+  overflow: 'hidden',
+  color: '#cff8fa',
+  background: '#13242b',
+  borderTop: "solid .05vw #2b3d43",
+  borderBottom: "solid .05vw #2b3d43",
+  borderRadius: '0px',
+  paddingBottom: '.2vh'
 };
 
 function callback(key) {
@@ -92,24 +102,26 @@ export default class Level extends React.Component {
   render() {
     return (
       <div style={{backgroundColor: "#13242b", height:"100vh"}}>
-      <div style={{height: "8vh", backgroundColor: "#13242b"}}>
-      <h1 style={{color: "#cff8fa", textAlign: "center", paddingTop: "2vh"}}>CRYPTEX</h1>
+      <div className="navb" style={{height: "8vh", backgroundColor: "#13242b"}}>
+      <a className="nav_link"><h3 style={{color:'#cff8fa', fontSize:'1vw'}}>RULES</h3></a>
+      <h3 style={{color: "#1dcad1", fontSize:'1.7vw'}}>CRYPTEX</h3>
+      <a className="nav_link"><h3 style={{color:'#cff8fa', fontSize:' 1vw'}}>PREVIOUS Qs</h3></a>
       </div>
-      <Row>
-      <Col span={5}>
-      <Collapse className= "acc" style={{ borderRadius: "0px"}} onChange={callback} expandIcon={({ isActive }) => <Icon type="caret-down" rotate={isActive ? 180 : 0} />} >
+      <Row style={{paddingTop:'4vh'}}>
+      <Col span={6}>
+      <Collapse defaultActiveKey={['1']} className= "acc" style={{ borderRadius: "0px"}} onChange={callback} expandIcon={({ isActive }) => <Icon type="caret-down" rotate={isActive ? 180 : 0} />} >
       <Panel className=" acc one" header="LEADERBOARD" key="1" style={customPanelStyle1}>
       <a href="https://www.helpnetsecurity.com/2016/05/12/facebook-ctf-platform-open-source/" target="_blank">
       <Table  className="borders" dataSource={data} pagination={false} showHeader={false}>
-      <Column className="colm" title="Rank" dataIndex="firstName" key="firstName" />
-      <Column className="colm" title="Username" dataIndex="lastName" key="lastName" />
-      <Column className="colm" title="Level" dataIndex="age" key="age" />
+      <Column className="colm" ellipsis={true} title="Rank" dataIndex="firstName" key="firstName" />
+      <Column className="colm" ellipsis={true} title="Username" dataIndex="lastName" key="lastName" />
+      <Column className="colm" ellipsis={true} title="Level" dataIndex="age" key="age" />
       </Table>
       </a>
       </Panel>
       <Panel className="acc two" header="COUNTDOWN" key="2" style={customPanelStyle2}>
       <p className="borders" style={{backgroundColor:"#13242b", textAlign:'center', color:'#cff8fa'}}>
-      <Row>
+      <Row style={{paddingTop:'1vw', paddingBottom: '1vw'}}>
       <Col span={8}>
       <p style={{fontSize:'1.3vw'}}>48</p>
       <p style={{fontSize:'.7vw'}}>HOURS</p>
@@ -127,11 +139,12 @@ export default class Level extends React.Component {
       </Panel>
       </Collapse>
       </Col>
-      <Col span={13}></Col>
+      <Col span={12}></Col>
       <Col span={6}>
-      <Collapse className="acc" style={{ borderRadius: "0px"}} onChange={call} expandIcon={({ isActive }) => <Icon type="caret-down" rotate={isActive ? 180 : 0} />} >
-      <Panel className="acc three" header="HINTS" key="1" style={customPanelStyle1}>
+      <Collapse defaultActiveKey={['1']} className="acc" style={{ borderRadius: "0px"}} onChange={call} expandIcon={({ isActive }) => <Icon type="caret-down" rotate={isActive ? 180 : 0} />} >
+      <Panel className="acc three" header="HINTS" key="1" style={customPanelStyle3}>
       <List
+      className="borders hints"
       itemLayout="horizontal"
       dataSource={new_data}
       renderItem={item => (
