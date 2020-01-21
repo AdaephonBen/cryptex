787bb986ef1e07e465fcbbf0a37a92e9821a28f5
deepikapalagani <es18btech11002@iith.ac.in>
2020-01-21 18:50:28 +0530

    updated

--- src/Components/LevelTemplates/index.js
+++ src/Components/LevelTemplates/index.js
@@ -1,120 +1,132 @@
 import React from "react";
-import { Layout, Menu, Timeline} from "antd";
-import { Row, Col, Table , Drawer, Icon} from 'antd';
 import Countdown from "react-houston";
 import "react-houston/dist/css/houston.css";
 import "./index.css";
-const { Header, Content} = Layout;
-const cb = () => {
-  console.log("expired callback");
-};
-const { Column} = Table;
+import { Collapse, Table, Row, Col, Icon, List } from 'antd';
+import Header from "antd/lib/calendar/Header";
+const { Panel } = Collapse;
+const { Column } = Table;
+
 const data = [
   {
     key: '1',
-    firstName: 'John',
-    lastName: 1,
+    firstName: 1,
+    lastName: 'Brown',
     age: 32,
   },
   {
     key: '2',
-    firstName: 'Jim',
-    lastName: 2,
+    firstName: 2,
+    lastName: 'Green',
     age: 42,
   },
   {
     key: '3',
-    firstName: 'Joe',
-    lastName: 3,
+    firstName: 3,
+    lastName: 'Black',
+    age: 32,
+  },
+  {
+    key: '4',
+    firstName: 4,
+    lastName: 'Black',
+    age: 32,
+  },
+  {
+    key: '5',
+    firstName: 5,
+    lastName: 'Black',
+    age: 32,
+  },
+  {
+    key: '6',
+    firstName: 6,
+    lastName: 'Black',
     age: 32,
   },
 ];
 
+const new_data = [
+  {
+    title: 'Ant Design Title 1',
+    description: 'yppppppppppppppppppppppppppppppp',
+  },
+  {
+    title: 'Ant Design Title 2',
+    description: 'yppppppppppppppppppppppppppppppp',
+  },
+  {
+    title: 'Ant Design Title 3',
+    description: 'yppppppppppppppppppppppppppppppp',
+  },
+  {
+    title: 'Ant Design Title 4',
+    description: 'yppppppppppppppppppppppppppppppp',
+  },
+];
+
+const customPanelStyle = {
+  overflow: 'hidden',
+  color: '#cff8fa',
+  background: '#13242b',
+};
+
+function callback(key) {
+  console.log(key);
+}
+
+function call(key) {
+  console.log(key);
+}
+
 export default class Level extends React.Component {
-  state = { visible: 'true', placement: 'left' };
-  
-  showDrawer = () => {
-    this.setState({
-      visible: true,
-    });
-  };
-  
-  onClose = () => {
-    this.setState({
-      visible: false,
-    });
-  };
-  
-  onChange = e => {
-    this.setState({
-      placement: e.target.value,
-    });
-  };
   render() {
     return (
-      <div>
-      <Layout className="layout" theme="dark" style={{paddingRight:'0px'}}>
-      <Header style={{ position: "fixed", zIndex: 1, width: "100%"}}>
-      <Menu
-      mode="horizontal"
-      style={{ lineHeight: '9vh', float:"right", backgroundColor: "#001529", fontSize: '2.2vh'}}
-      >
-      <Menu.Item  key="1" onClick={this.showDrawer}><a id="link_one"><Icon type="tool" />Utilities</a></Menu.Item>
-      <Menu.Item  key="2"><a id="link_two"href="https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_css_width_ref"  target="_blank">
-      <Icon type="bars" />Rules</a></Menu.Item>
-      <Menu.Item  key="3" ><a id="link_three"href="https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_css_width_ref"  target="_blank">
-      <Icon type="trophy" />Leaderboard</a></Menu.Item>
-      </Menu>
-      </Header>
-      <Content style={{ padding: '0 50px'}}>
-      <Drawer
-      title="CRYPTEX"
-      placement={this.state.placement}
-      onClose={this.onClose}
-      visible={this.state.visible}
-      mask={false}
-      maskClosable={false}
-      width="18vw"
-      >
-      <a href="https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_css_width_ref"  target="_blank"><Table dataSource={data} pagination={false}>
-      <Column title="Rank" dataIndex="lastName" key="lastName" />
-      <Column title="Name" dataIndex="firstName" key="firstName" />
-      <Column title="Level" dataIndex="age" key="age" />
-      />
-      </Table></a>
-      <Countdown endDate={new Date("January 24, 2020 00:00:00")} />
-      </Drawer>
+      <div style={{backgroundColor: "#13242b", height:"100vh"}}>
+      <div style={{height: "8vh", backgroundColor: "#13242b"}}>
+      <h1 style={{color: "#cff8fa", textAlign: "center", paddingTop: "2vh"}}>CRYPTEX</h1>
+      </div>
       <Row>
-      <Col span={18} id="col2" style={{height:"100vh", padding:"10vh 2vw"}}>
+      <Col span={5}>
+      <Collapse className= "acc" style={{ borderRadius: "0px"}} onChange={callback} expandIcon={({ isActive }) => <Icon type="caret-down" rotate={isActive ? 180 : 0} />} >
+      <Panel className=" acc one" header="LEADERBOARD" key="1" style={customPanelStyle}>
+      <a href="https://www.helpnetsecurity.com/2016/05/12/facebook-ctf-platform-open-source/" target="_blank">
+      <Table  className="borders" dataSource={data} pagination={false} showHeader={false}>
+      <Column className="colm" title="Rank" dataIndex="firstName" key="firstName" />
+      <Column className="colm" title="Username" dataIndex="lastName" key="lastName" />
+      <Column className="colm" title="Level" dataIndex="age" key="age" />
+      </Table>
+      </a>
+      </Panel>
+      <Panel className="acc two" header="COUNTDOWN" key="2" style={customPanelStyle}>
+      <p className="borders" style={{textAlign:"center", backgroundColor:"#13242b"}}><Countdown endDate={new Date('January 26, 2020 00:00:00')} /></p>
+      </Panel>
+      </Collapse>
       </Col>
-      <Col span={6} id= "col3" style={{height:"100vh", padding:"10vh 2vw", borderLeft:".1vw #4f5254 solid"}}>
-      <h2 align="center" style={{color: "white", marginBottom: '5vh'}}><Icon type="bulb" /> HINTS</h2>
-      <div style={{color:'white'}}>
-      <Timeline >
-      <Timeline.Item color="#0faf9f">
-      Hint 1
-      <p>Create a services site 2015-09-01 big big big big big big big big big big gib </p>
-      </Timeline.Item>
-      <Timeline.Item color="#0faf9f">
-      Hint 2
-      <p>Solve initial network problems 2015-09-01</p>
-      </Timeline.Item>
-      <Timeline.Item color="#0faf9f">
-      Hint 3
-      <p>Technical testing 2015-09-01</p>
-      </Timeline.Item>
-      <Timeline.Item color="#0faf9f">
-      Hint 4
-      <p>Network problems being solved 2015-09-01</p>
-      </Timeline.Item>
-      </Timeline>
-      </div>
-      </Col>
-      </Row>
-      </Content>
-      </Layout>
-      </div>
-      );
+      <Col span={13}></Col>
+      <Col span={6}>
+      <Collapse className="acc" style={{ borderRadius: "0px"}} onChange={call} expandIcon={({ isActive }) => <Icon type="caret-down" rotate={isActive ? 180 : 0} />} >
+      <Panel className="acc three" header="HINTS" key="1" style={customPanelStyle}>
+      <List
+      itemLayout="horizontal"
+      dataSource={new_data}
+      renderItem={item => (
+        <List.Item className="lists">
+        <List.Item.Meta className="lists"
+        avatar={<Icon style={{color:'#cff8fa', fontSize:'2vw'}} type="sketch"/>}
+        title={item.title}
+        description={item.description}
+        />
+        </List.Item>
+        )}
+        />
+        </Panel>
+        </Collapse>
+        </Col>
+        </Row>
+        </div>
+        );
+      }
     }
-  }
-  
+    
+    
