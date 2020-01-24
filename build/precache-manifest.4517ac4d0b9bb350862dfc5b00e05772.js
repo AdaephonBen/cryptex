3541da9c25f772ace9fbb0521575e4704354e69f
AdaephonBen <ma18btech11011@iith.ac.in>
2020-01-24 20:40:56 +0530

    Add gitignore

---
+++ build/precache-manifest.4517ac4d0b9bb350862dfc5b00e05772.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d486c54625a61d352543d2e58babbaa6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d340bdc58a89a5252f8a",
+    "url": "/static/css/2.4d33d220.chunk.css"
+  },
+  {
+    "revision": "468f67f2e17cb46e95e4",
+    "url": "/static/css/main.4baaf21b.chunk.css"
+  },
+  {
+    "revision": "d340bdc58a89a5252f8a",
+    "url": "/static/js/2.74e50312.chunk.js"
+  },
+  {
+    "revision": "5cdd374980f7418559befa792a5d58c0",
+    "url": "/static/js/2.74e50312.chunk.js.LICENSE"
+  },
+  {
+    "revision": "468f67f2e17cb46e95e4",
+    "url": "/static/js/main.89aff93b.chunk.js"
+  },
+  {
+    "revision": "282839b5ae3a7d34d43c",
+    "url": "/static/js/runtime-main.5ca447cf.js"
+  },
+  {
+    "revision": "87e7ea2ce3d1957dee5c08ec5e9b5189",
+    "url": "/static/media/128x128.87e7ea2c.png"
+  },
+  {
+    "revision": "90e17d4876db363bbd6a10516c955b40",
+    "url": "/static/media/bg.90e17d48.jpg"
+  },
+  {
+    "revision": "dd750d5d9bf0f1ef3a6d04985459fbbb",
+    "url": "/static/media/bg.dd750d5d.png"
+  }
+]);
