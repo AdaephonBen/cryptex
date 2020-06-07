b1b62abba7afdec1d125d7c16670fb358c79ef0c
AdaephonBen <ma18btech11011@iith.ac.in>
2020-06-07 23:24:46 +0530

    Fixed all linting errors and warnings

--- client/src/Components/MiniLeaderboard/MiniLeaderboard.js
+++ client/src/Components/MiniLeaderboard/MiniLeaderboard.js
@@ -1,97 +1,34 @@
 import React from "react";
 import { Flex, Text, Link, Tooltip } from "@chakra-ui/core";
-import {
-  FaSyncAlt,
-  FaExternalLinkAlt,
-  FaAngleUp,
-  FaAngleDown,
-} from "react-icons/fa";
+import { FaSyncAlt, FaExternalLinkAlt } from "react-icons/fa";
+import Table from "./Table";
 import "./styles.css";
 
-class Table extends React.Component {
-  render() {
-    return (
-      <table className="mini-leaderboard-table">
-        <tr>
-          <th>Rank</th>
-          <th>Username</th>
-          <th>Level</th>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>adaephonben</td>
-          <td>20</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>adaephonben</td>
-          <td>20</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>adaephonben</td>
-          <td>20</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>adaephonben</td>
-          <td>20</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>adaephonben</td>
-          <td>20</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>adaephonben</td>
-          <td>20</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>adaephonben</td>
-          <td>20</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>adaephonben</td>
-          <td>20</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>adaephonben</td>
-          <td>20</td>
-        </tr>
-      </table>
-    );
-  }
-}
-
-export default class MiniLeaderboard extends React.Component {
-  render() {
-    return (
-      <Flex
-        className="mini-leaderboard"
-        flexDirection="column"
-        backgroundColor="#212837"
-      >
-        <Flex className="leaderboard-header" justifyContent="space-between">
-          <Text className="leaderboard-title">Leaderboard</Text>
-          <Flex className="leaderboard-buttons">
-            <Tooltip label="Refresh leaderboard">
-              <Link href="http://google.com" className="icon-button">
-                <FaSyncAlt></FaSyncAlt>
-              </Link>
-            </Tooltip>
-            <Tooltip label="View complete leaderboard">
-              <Link href="http://google.com" className="icon-button">
-                <FaExternalLinkAlt></FaExternalLinkAlt>
-              </Link>
-            </Tooltip>
-          </Flex>
+const MiniLeaderboard = () => {
+  return (
+    <Flex
+      className="mini-leaderboard"
+      flexDirection="column"
+      backgroundColor="#212837"
+    >
+      <Flex className="leaderboard-header" justifyContent="space-between">
+        <Text className="leaderboard-title">Leaderboard</Text>
+        <Flex className="leaderboard-buttons">
+          <Tooltip label="Refresh leaderboard">
+            <Link href="http://google.com" className="icon-button">
+              <FaSyncAlt />
+            </Link>
+          </Tooltip>
+          <Tooltip label="View complete leaderboard">
+            <Link href="http://google.com" className="icon-button">
+              <FaExternalLinkAlt />
+            </Link>
+          </Tooltip>
         </Flex>
-        <Table />
       </Flex>
-    );
-  }
-}
+      <Table />
+    </Flex>
+  );
+};
+
+export default MiniLeaderboard;
---
+++ client/src/Components/MiniLeaderboard/Table.js
@@ -0,0 +1,61 @@
+import React from "react";
+import "./styles.css";
+
+const Table = () => {
+  return (
+    <table className="mini-leaderboard-table">
+      <tr>
+        <th>Rank</th>
+        <th>Username</th>
+        <th>Level</th>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>adaephonben</td>
+        <td>20</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>adaephonben</td>
+        <td>20</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>adaephonben</td>
+        <td>20</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>adaephonben</td>
+        <td>20</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>adaephonben</td>
+        <td>20</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>adaephonben</td>
+        <td>20</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>adaephonben</td>
+        <td>20</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>adaephonben</td>
+        <td>20</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>adaephonben</td>
+        <td>20</td>
+      </tr>
+    </table>
+  );
+};
+
+export default Table;
