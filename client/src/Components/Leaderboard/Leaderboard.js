988835c9395d629a0ebbed8ad8805ff7c5548627
Rachit Keerti Das <rachitkdas@gmail.com>
2020-06-14 12:25:03 +0530

    Refactor Table.Add OnCLick functionality

--- client/src/Components/Leaderboard/Leaderboard.js
+++ client/src/Components/Leaderboard/Leaderboard.js
@@ -1,44 +1,59 @@
 import React from "react";
-import { FaAngleLeft,FaAngleRight,FaSearch } from "react-icons/fa";
-import {Flex, 
-    Box,
-    Divider,
-    Input,
-    FormControl,
-    IconButton,
-    Heading} from "@chakra-ui/core"
+import { FaAngleLeft, FaAngleRight, FaSearch } from "react-icons/fa";
+import {
+  Flex,
+  Box,
+  Divider,
+  Input,
+  FormControl,
+  IconButton,
+  Heading,
+} from "@chakra-ui/core";
 import Navbar from "../Navbar/Navbar";
 import Table from "./table";
-import "./styles.css"
+import "./styles.css";
 // import { icons } from "react-icons/lib/cjs";
 
-function Leaderboard () {
-    return (
-        <section className="page">
-        <Navbar/>
-        <Flex align="center" justify="space-between" flexDirection="column" className="leaderboard">
-        <Heading marginTop="1.5rem" className="heading">
-            LEADERBOARD
-            <Divider />
+function Leaderboard() {
+  return (
+    <Box className="page">
+      <Navbar />
+      <Flex
+        align="center"
+        justify="space-between"
+        flexDirection="column"
+        className="leaderboard"
+      >
+        <Heading
+          marginTop="1.5rem"
+          letterSpacing={[3, 7, 8]}
+          fontWeight={[520, 600, 600]}
+          marginRight={["-3", "-7", "-8"]}
+          className="heading"
+        >
+          LEADERBOARD
+          <Divider />
         </Heading>
         <Flex className="filters">
-            <FormControl>
-                <Input placeholder="Team/User Name" size="sm"/>
-            </FormControl>
-            <IconButton icon={FaSearch} size="sm"/>
+          <FormControl>
+            <Input placeholder="Team/User Name" size="sm" />
+          </FormControl>
+          <IconButton
+            aria-label="Search"
+            icon={FaSearch}
+            size="sm"
+            className="search"
+          />
         </Flex>
         <Table />
-            <Box className="paginator">
-                
-            <IconButton icon={FaAngleLeft} size="sm"/>
-            &nbsp;Page 1 of 20&nbsp;
-            <IconButton icon={FaAngleRight} size="sm"/>
-
+        <Box className="paginator">
+          <IconButton icon={FaAngleLeft} size="sm" />
+          &nbsp;Page 1 of 20&nbsp;
+          <IconButton icon={FaAngleRight} size="sm" />
         </Box>
-        
-        </Flex>
-        </section>
-    );
+      </Flex>
+    </Box>
+  );
 }
 
-    export default Leaderboard;
+export default Leaderboard;
--- client/src/Components/Leaderboard/table.js
+++ client/src/Components/Leaderboard/table.js
@@ -1,60 +1,92 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Text, Collapse, Button } from '@chakra-ui/core';
+import './styles.css';
 
-function Table () {
-    return(
-            <table className="table">
-                <tr>
-                    <th>Rank</th>
-                    <th>Team</th>
-                    <th>Level</th>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>adaephonben</td>
-                    <td>20</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>adaephonben</td>
-                    <td>20</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>adaephonben</td>
-                    <td>20</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>adaephonben</td>
-                    <td>20</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>ChilledMustang</td>
-                    <td>20</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>adaephonben</td>
-                    <td>20</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>adaephonben</td>
-                    <td>20</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>adaephonben</td>
-                    <td>20</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>adaephonben</td>
-                    <td>20</td>
-                </tr>
-            </table>
-    );
+function Record(props) {
+  const [show, setShow] = React.useState(false);
+  const handleToggle = function () {
+    return setShow(!show);
+  };
+  const members = props.users.map((user) => <Text>{user}</Text>);
+  return (
+    <tr className="record">
+      <td>{props.rank}</td>
+      <td>
+        <Collapse isOpen={show} startingHeight={25}>
+          <Button variant="link" onClick={handleToggle} marginBottom="8px">
+            {props.team}
+          </Button>
+          {members}
+        </Collapse>
+      </td>
+      <td>20</td>
+    </tr>
+  );
+}
+
+function Table() {
+  return (
+    <table className="table">
+      <tr>
+        <th>Rank</th>
+        <th>Team</th>
+        <th>Level</th>
+      </tr>
+      <tbody>
+        <Record
+          rank="2"
+          team="adaephonben"
+          users={['RonaldRoe', 'JaneDoe', 'RonaldRoe', 'JaneDoe']}
+        />
+        <Record
+          rank="2"
+          team="adaephonben"
+          users={['RonaldRoe', 'JaneDoe', 'RonaldRoe', 'JaneDoe']}
+        />
+        <Record
+          rank="2"
+          team="adaephonben"
+          users={['RonaldRoe', 'JaneDoe', 'RonaldRoe', 'JaneDoe']}
+        />
+        <Record
+          rank="2"
+          team="adaephonben"
+          users={['RonaldRoe', 'JaneDoe', 'RonaldRoe', 'JaneDoe']}
+        />
+        <Record
+          rank="2"
+          team="adaephonben"
+          users={['RonaldRoe', 'JaneDoe', 'RonaldRoe', 'JaneDoe']}
+        />
+        <Record
+          rank="2"
+          team="adaephonben"
+          users={['RonaldRoe', 'JaneDoe', 'RonaldRoe', 'JaneDoe']}
+        />
+        <Record
+          rank="2"
+          team="adaephonben"
+          users={['RonaldRoe', 'JaneDoe', 'RonaldRoe', 'JaneDoe']}
+        />
+        <Record
+          rank="2"
+          team="adaephonben"
+          users={['RonaldRoe', 'JaneDoe', 'RonaldRoe', 'JaneDoe']}
+        />
+        <Record
+          rank="2"
+          team="adaephonben"
+          users={['RonaldRoe', 'JaneDoe', 'RonaldRoe', 'JaneDoe']}
+        />
+        <Record
+          rank="2"
+          team="adaephonben"
+          users={['RonaldRoe', 'JaneDoe', 'RonaldRoe', 'JaneDoe']}
+        />
+      </tbody>
+    </table>
+  );
 }
 
 export default Table;
