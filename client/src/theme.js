1e258816cf48e1c21924c1501b752ef852d758c7
AdaephonBen <ma18btech11011@iith.ac.in>
2021-03-05 10:12:50 +0530

    Navbar fixed
    Signed-off-by: AdaephonBen <ma18btech11011@iith.ac.in>

--- client/src/theme.js
+++ client/src/theme.js
@@ -1,8 +1,15 @@
 import { extendTheme } from "@chakra-ui/react";
 import { mode } from "@chakra-ui/theme-tools";
+import { createBreakpoints } from "@chakra-ui/theme-tools";
 
 // Let's say you want to add custom colors
 const customTheme = extendTheme({
+  breakpoints: createBreakpoints({
+    sm: "360px",
+    md: "768px",
+    lg: "1024px",
+    xl: "1440px",
+  }),
   styles: {
     global: (props) => ({
       body: {
